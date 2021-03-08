8dca66722aa4be1806f3d5b3b4aa4e25da4df795
Marlon Martins <marlon-martins@outlook.com>
2021-03-09 00:00:03 +0100

    UPDATE JavaScript DOM

--- 06 JavaScript DOM/1- The Document Object Model/5- Adding and removing class/app.js
+++ 06 JavaScript DOM/1- The Document Object Model/5- Adding and removing class/app.js
@@ -1,7 +1,27 @@
-const content = document.querySelector('p');
+// const content = document.querySelector('p');
 
-console.log(content.classList);
+// console.log(content.classList);
 
-content.classList.add('error');
-content.classList.remove('error');
-content.classList.add('success');
+// content.classList.add('error');
+
+// content.classList.remove('error');
+
+// content.classList.add('success');
+
+const paras = document.querySelectorAll('p');
+
+paras.forEach(p => {
+  if(p.textContent.includes('error')){
+    p.classList.add('error');
+  } else if(p.textContent.includes('success')) {
+    p.classList.add('success');
+  }
+});
+
+const title = document.querySelector('test');
+
+// It will give a class test to title(h1)
+title.classList.toggle('test');
+
+// // It will remove a class test to title(h1)
+title.classList.toggle('test');
