cff08a23dfa91c3e51094f22d8b8f305fe88a6b3
Marlon Martins <marlon-martins@outlook.com>
2021-06-16 19:35:55 +0200

    Add files via upload

--- 6- Repeating Stuff With Loops/1- Intro to For Loops/app.js
+++ 6- Repeating Stuff With Loops/1- Intro to For Loops/app.js
@@ -14,6 +14,9 @@
 
 // ======================================
 // OUR FIRST FOR LOOP
+// Print numbers from 1 to 10
+// let num = 1; num <= 10; num += 1
+// start at 1; stop at 10; add 1 each time
 // ======================================
 
 for (let num = 1; num <= 10; num += 1) {
@@ -24,24 +27,47 @@ for (let num = 1; num <= 10; num += 1) {
 // MORE EXAMPLES OF FOR LOOPS!
 // ======================================
 
-// for (let i = 1; i <= 20; i += 2) {
-//     console.log(i)
-// }
+// Print out "Da ba dee da ba daa" 6 times, using a for loop
+for (let i = 1; i <= 6; i++) {
+    console.log("Da ba dee da ba da");
+}
 
-// for (let i = 100; i >= 0; i -= 10) {
-//     console.log(i);
-// }
+// Print out even numbers from 2 to 20
+for (let e = 2; e <= 20; e += 2) {
+    console.log(e)
+    console.log('even numbers');
+}
 
-// for (let i = 10; i <= 1000; i *= 10) {
-//     console.log(i);
-// }
+// Print out odd numbers from 1 to 19
+for (let o = 1; o <= 20; o += 2) {
+    console.log(o)
+    console.log('odd numbers');
+}
+
+// Count down starting from 100 to 10
+for (let c = 100; c >= 10; c -= 10) {
+    console.log(c);
+    console.log('count down 100 to 10');
+}
+
+// Multiply 10*10 until you get 1000
+for (let m = 10; m <= 1000; m *= 10) {
+    console.log(m);
+}
+
+// Write a loop that prints: 25, 20, 15, 10, 5, 0
+// start at 25; stop at 0; deducts 5 each time
+for (let f = 25; f >= 0; f -= 5) {
+    console.log(f);
+    console.log('25-20-15-10-5-0');
+}
 
 // DO NOT RUN THIS LOOP
 // INFINITE LOOP!! BAD!!
 // ================================
 // ================================
 // for (let i = 20; i >= 0; i++) {
-//     console.log(i)
+//     console.log(i);
 // }
 // ================================
 // ================================
--- 6- Repeating Stuff With Loops/3- Nested Loops/nestedLoops.js
+++ 6- Repeating Stuff With Loops/3- Nested Loops/nestedLoops.js
@@ -2,12 +2,26 @@
 // NESTED FOR LOOP EXAMPLES
 // ===================================
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 3; i++) {
     console.log(`i is: ${i}`)
-    for (let j = 1; j < 4; j++) {
+    for (let j = 1; j <= 3; j++) {
         console.log(`     j is: ${j}`)
     }
 }
+/*
+1
+ 1
+ 2
+ 3
+2
+ 1
+ 2
+ 3 
+3
+ 1
+ 2
+ 3 
+*/
 
 // ===================================
 // NESTED ARRAY ITERATION
@@ -22,7 +36,7 @@ const seatingChart = [
 
 for (let i = 0; i < seatingChart.length; i++) {
     const row = seatingChart[i];
-    console.log(`ROW #${i + 1}`)
+    console.log(`ROW #${i + 1}`) // Add one because index starts at zero
     for (let j = 0; j < row.length; j++) {
         console.log(row[j])
     }
