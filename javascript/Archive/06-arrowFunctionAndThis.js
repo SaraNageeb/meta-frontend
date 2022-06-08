139006de4e59a9811251fbdc82727d1956c0a0c0
Marlon Martins <marlon-martins@outlook.com>
2022-06-08 17:27:29 +0200

    Update 06-arrowFunctionAndThis.js

--- javascript/Archive/06-arrowFunctionAndThis.js
+++ javascript/Archive/06-arrowFunctionAndThis.js
@@ -1,3 +1,4 @@
+// Regular function with this keyword
 const cars = {
     model: 'Audi',
     price: 5000,
@@ -6,4 +7,25 @@ const cars = {
     }
 };
 
-console.log(cars.description());
+console.log(cars.description()); // "The model and price of this car: Audi - 5000"
+
+
+// Arrow function with this keyword
+const player = {
+  name: 'Cristiano Ronaldo',
+  worldCupPlayed: [2006, 2010, 2014, 2018, 2022],
+  showWCP() {
+    this.worldCupPlayed.forEach((worldCup) => {
+      console.log(this.name + " played at World Cup " + worldCup);
+    });  
+  }
+};
+
+player.showWCP();
+/*
+"Cristiano Ronaldo played at World Cup 2006"
+"Cristiano Ronaldo played at World Cup 2010"
+"Cristiano Ronaldo played at World Cup 2014"
+"Cristiano Ronaldo played at World Cup 2018"
+"Cristiano Ronaldo played at World Cup 2022"
+*/
