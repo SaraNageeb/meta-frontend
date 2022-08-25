0540588bbe20f5b00b68b6abe5874599f093e5dd
Marlon Martins <marlon-martins@outlook.com>
2022-08-25 13:25:23 +0200

    Update 2-array_interview_questions.js

--- 2- Programming with JavaScript/8- Arrays/exercises/2-array_interview_questions.js
+++ 2- Programming with JavaScript/8- Arrays/exercises/2-array_interview_questions.js
@@ -1,8 +1,26 @@
 /*
-  01
-
-  - Gere um novo array com apenas os números ímpares do array abaixo e exiba 
-    o novo array no console.
-
-  Dica: pesquise por remainder operator (%).
+ 01
+Print the value, index, array from a given array
 */
+
+const numbers = [1,2,3,4,5];
+
+numbers.forEach((number,index,array) => {
+console.log(`Index: ${index} - ${number} - ${array}`);
+})
+
+/*
+ 02
+Print the value, index, array from a given array
+*/
+
+/*
+ 01
+Count the Duplicates in an Array #
+To count the duplicates in an array:
+
+1. Declare an empty object variable that will store the count for each value.
+2. Use the forEach() method to iterate over the array.
+3. On each iteration, increment the count for the value by 1 or initialize it to 1.
+*/
+
