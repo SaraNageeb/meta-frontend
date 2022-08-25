218d06b1216ff38d26ec0026b43a32ef8f3b6edf
Marlon Martins <marlon-martins@outlook.com>
2022-08-25 14:30:44 +0200

    Update 2-array_interview_questions.js

--- 2- Programming with JavaScript/8- Arrays/exercises/2-array_interview_questions.js
+++ 2- Programming with JavaScript/8- Arrays/exercises/2-array_interview_questions.js
@@ -23,6 +23,21 @@ console.log(sum);
 /*
  03
 Check how many times an item appears in an array
+
+To count the duplicates in an array:
+
+1.Declare an empty object variable that will store the count for each value.
+2.Use the forEach() method to iterate over the array.
+3.On each iteration, increment the count for the value by 1 or initialize it to 1.
+
+
+The function we passed to the Array.forEach method gets called with each element in the array.
+
+On each iteration, we check if the current value is already present as a key in the count object. If it is present, we increment it by 1.
+
+If the value is not present, we initialize it to 1.
+
+We used the logical OR (||) operator to check if the key has not been initialized in the array. If the accessor returns undefined, we initialize the value for the key to 0 + 1.
 */
 
 const countries = ['Argentina', 'Brazil', 'Argentina', 'Portugal', 'Brazil'];
