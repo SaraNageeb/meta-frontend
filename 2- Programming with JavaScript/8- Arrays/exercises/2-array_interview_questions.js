32720ae90bc323f4035058c06e236712d1019328
Marlon Martins <marlon-martins@outlook.com>
2022-08-25 14:29:15 +0200

    Update 2-array_interview_questions.js

--- 2- Programming with JavaScript/8- Arrays/exercises/2-array_interview_questions.js
+++ 2- Programming with JavaScript/8- Arrays/exercises/2-array_interview_questions.js
@@ -11,16 +11,29 @@ console.log(`Index: ${index} - ${number} - ${array}`);
 
 /*
  02
-Print the value, index, array from a given array
+Calculcate the sum of the numbers from the Array
 */
+
+const numbers = [1,2,3,4,5];
+let sum = 0;
+
+numbers.forEach(item => sum += item);
+console.log(sum);
 
 /*
- 01
-Count the Duplicates in an Array #
-To count the duplicates in an array:
-
-1. Declare an empty object variable that will store the count for each value.
-2. Use the forEach() method to iterate over the array.
-3. On each iteration, increment the count for the value by 1 or initialize it to 1.
+ 03
+Check how many times an item appears in an array
 */
+
+const countries = ['Argentina', 'Brazil', 'Argentina', 'Portugal', 'Brazil'];
+
+// Keep track count of every country
+let count = {};
+
+
+countries.forEach((country) => {
+    count[country] = (count[country] || 0) + 1;
+})
+
+console.log(count);
 
