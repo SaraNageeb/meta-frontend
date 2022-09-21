8d787fbea466d44e9bdd1d18650fc63cbac6a4ff
Marlon Martins <marlon-martins@outlook.com>
2022-09-21 10:21:54 +0200

    Delete

--- 2- Programming with JavaScript/10- Functions/2- javascript functions/3 – Arrow Functions/3-Default Function Parameters/js/arrow-functions.js
+++
@@ -1,9 +0,0 @@
-const getRandomNumber = (upper) => {
-  const randomNumber = Math.floor( Math.random() * upper ) + 1;
-  return randomNumber;
-};
-
-const getArea = (width, length, unit) => {
-  const area = width * length;
-  return `${area} ${unit}`;
-};
--- 2- Programming with JavaScript/10- Functions/2- javascript functions/3 – Arrow Functions/3-Default Function Parameters/js/default-params.js
+++
@@ -1,8 +0,0 @@
-function sayGreeting(greeting = 'Good morning', name = 'student') {
-  return `${greeting}, ${name}!`;
-}
-
-function getArea(width, length, unit) {
-  const area = width * length;
-  return `${area} ${unit}`;
-}
